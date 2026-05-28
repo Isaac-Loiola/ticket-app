2a6a41d4cbef09398c84538827533e21666ff023
Isaac-Loiola <isaackopi@icloud.com>
2026-05-28 14:59:29 -0300

    Initial folders added!

---
+++ src/app/pages/totem/totem.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-totem',
+  imports: [],
+  templateUrl: './totem.html',
+  styleUrl: './totem.css',
+})
+export class Totem {}
