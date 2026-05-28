e6f19fc2353afa0d96a35e9cdeb5b36d190c89eb
Isaac-Loiola <isaackopi@icloud.com>
2026-05-28 18:35:51 -0300

    Totem for monitor screen added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Panel } from './pages/panel/panel';
+import { Totem } from './pages/totem/totem';
 
 export const routes: Routes = [
-    {path: '', component:Panel}
+    {path: '', component:Panel},
+    {path: 'totem', component:Totem}
 ];
