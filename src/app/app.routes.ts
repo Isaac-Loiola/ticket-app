bf617fcbc6aed96d01db727115796bae9181c862
Isaac-Loiola <isaackopi@icloud.com>
2026-05-28 22:20:33 -0300

    Elements added to attendance page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Panel } from './pages/panel/panel';
 import { Totem } from './pages/totem/totem';
+import { Attendance } from './pages/attendance/attendance';
 
 export const routes: Routes = [
     {path: '', component:Panel},
-    {path: 'totem', component:Totem}
-];
+    {path: 'totem', component:Totem},
+    {path: 'call', component:Attendance},
+]
